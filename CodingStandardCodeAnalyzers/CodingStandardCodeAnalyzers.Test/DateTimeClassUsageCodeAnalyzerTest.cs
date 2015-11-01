6f2b0469446d8858865de3d4db600165175f5148
Oleksii Temekunidi <vuderr@gmail.com>
2015-11-01 13:59:11 +0200

    DateTimeClassUsageCodeAnalyzer changed to work with "using static System.DateTime"

---
+++ CodingStandardCodeAnalyzers/CodingStandardCodeAnalyzers.Test/DateTimeClassUsageCodeAnalyzerTest.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TestHelper;
+
+namespace CodingStandardCodeAnalyzers.Test {
+    [TestClass]
+    public class DateTimeClassUsageCodeAnalyzerTest : CodeFixVerifier {
+        #region Code samples
+        public static readonly string Wrong1 = @"
+using System;
+namespace DateTimeClassAnalyzerTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(DateTime.Now );  
+        }
+    }
+}";
+
+        public static readonly string Wrong2 = @"
+using System;
+using static System.DateTime;
+namespace DateTimeClassAnalyzerTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Now);  
+        }
+    }
+}";
+
+        public static readonly string Wrong3 = @"
+using System;
+namespace DateTimeClassAnalyzerTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(DateTime.Today );  
+        }
+    }
+}";
+
+        public static readonly string Correct1 = @"
+using System;
+namespace DateTimeClassAnalyzerTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(DateTimeOffset.Now);  
+        }
+    }
+}";
+
+        public static readonly string Correct2 = @"
+using System;
+namespace DateTimeClassAnalyzerTest {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(UtcNow);  
+        }
+    }
+}";
+        #endregion
+
+        [TestMethod]
+        public void NoDiagnosticsExpectedForEmptyLine() {
+            VerifyCSharpDiagnostic("");
+        }
+
+        [TestMethod]
+        public void UseOfDateTimeNowIsError() {
+            DiagnosticResult expected = CreateDiagnosticResult(6, 40, "Now");
+            VerifyCSharpDiagnostic(Wrong1, expected);
+        }
+
+        [TestMethod]
+        public void UseOfDateTimeNowAsStaticUsingIsError() {
+            DiagnosticResult expected = CreateDiagnosticResult(7, 31, "Now");
+            VerifyCSharpDiagnostic(Wrong2, expected);
+        }
+
+        [TestMethod]
+        public void UseOfDateTimeTodayIsError() {
+            DiagnosticResult expected = CreateDiagnosticResult(6, 40, "Today");
+            VerifyCSharpDiagnostic(Wrong3, expected);
+        }
+
+        [TestMethod]
+        public void UseOfNowPropertyInOtherClassIsCorrect() {
+            VerifyCSharpDiagnostic(Correct1);
+        }
+
+        [TestMethod]
+        public void UseOfOtherPropertyInDateTimeClassIsCorrect() {
+            VerifyCSharpDiagnostic(Correct2);
+        }
+        
+        private static DiagnosticResult CreateDiagnosticResult(int line, int column, string methodName) {
+            return new DiagnosticResult {
+                Id = "DateTimeClassUsageCodeAnalyzer",
+                Message = $"Use of 'DateTime.{methodName}' is not recommended. Consider other options to achive what you need.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", line, column) }
+            };
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+            return new DateTimeClassUsageCodeAnalyzer();
+        }
+    }
+}
---
+++ CodingStandardCodeAnalyzers/CodingStandardCodeAnalyzers/DateTimeClassUsageCodeAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodingStandardCodeAnalyzers {
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DateTimeClassUsageCodeAnalyzer : DiagnosticAnalyzer {
+        private static readonly LocalizableString Title = "Use of some methods of DateTime is not recommended.";
+        private static readonly LocalizableString MessageFormat = "Use of '{0}' is not recommended. Consider other options to achive what you need.";
+        private static readonly LocalizableString Description = "";
+        private static readonly string Category = "Coding pratices";
+        private static readonly string HelpLink = "http://tinyurl.com/CaseAgainsDateTimeNow";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor("DateTimeClassUsageCodeAnalyzer",
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: Description,
+            helpLinkUri: HelpLink);
+
+        public static readonly string DiagnosticId = "DateTimeClassUsageCodeAnalyzer";
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context) {
+            context.RegisterSyntaxNodeAction(AnalyzeDateTimeUsage, SyntaxKind.IdentifierName);
+        }
+
+        private void AnalyzeDateTimeUsage(SyntaxNodeAnalysisContext context) {
+            var expression = context.Node as IdentifierNameSyntax;
+            if (expression == null || context.SemanticModel == null) { return; }
+            SymbolInfo diagnostics = context.SemanticModel.GetSymbolInfo(expression);
+            ISymbol symbol = diagnostics.Symbol;
+            if (symbol?.ContainingType?.ContainingNamespace == null) { return; }
+            if (symbol.ContainingType.ContainingNamespace.Name == "System" && symbol.ContainingType.Name == "DateTime") {
+                if (symbol.Name == "Now" || symbol.Name == "Today") {
+                    Diagnostic diagnostic = Diagnostic.Create(Rule, expression.GetLocation(), $"{symbol.ContainingType.Name}.{symbol.Name}");
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
