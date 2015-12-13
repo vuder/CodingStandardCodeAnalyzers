10d2ec71ca0445417bb83dc40b12066065debd75
Oleksii Temekunidi <vuderr@gmail.com>
2015-12-13 15:30:54 +0200

    Added TimeMeasurement detection sample

---
+++ CodeSamples/TimeMeasurementSamples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples
+{
+    class TimeMeasurementSamples
+    {
+        public void Wrong1()
+        {
+            var start = DateTimeOffset.Now;
+            //do something
+            System.Threading.Thread.Sleep(1000);
+            var elapsed = DateTimeOffset.Now - start;
+        }
+    }
+}
